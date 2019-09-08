1fb42ab23666fdd245b85b1037246c134bcb9fd2
Alvaro Miranda Aguilera <kikitux@gmail.com>
2019-09-08 19:28:15 +0200

    update

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "vsphere_virtual_machine" "lb-vm" {
 }
 
 output "guest_ip_address" {
-  value = vsphere_virtual_machine.lb-vm.guest_ip_addresses[0]
+  value = vsphere_virtual_machine.lb-vm.guest_ip_addresses
 }
 
 output "name" {
